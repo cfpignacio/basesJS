d54fdc5c2fef373bf69abc847d474d990374ec18
Ignacio Camillucci <ignacio.camillucci@gmail.com>
2023-04-13 22:59:36 -0300

    ejercicios

---
+++ data/db2.js
@@ -0,0 +1,9 @@
+const usuarios = [
+	{ nombre: 'Juan', edad: 25, Email: 'juan@example.com' },
+	{ nombre: 'Ana', edad: 30, Email: 'ana@example.com' },
+	{ nombre: 'Pedro', edad: 28, Email: 'pedro@example.com' },
+	{ nombre: 'María', edad: 33, Email: 'maria@example.com' },
+	{ nombre: 'Luis', edad: 21, Email: 'luis@example.com' },
+];
+
+// crear funcion flecha findUserByEmail(email), la misma tiene que devolver un usuario
---
+++ ejercicios.js
@@ -0,0 +1,74 @@
+// El siguente ejercicio tiene un error lo que impide
+// imprimer por consola el listado, encontrar el error
+// y solucionarlo
+// const array = ["Java","Python","C","C++"]
+
+// for (const index = 0; index < array.length; index++) {
+//     const element = array[index];
+//     console.log(element)
+// }
+
+// salida esperada ->
+// Java
+// Python
+// C
+// C++
+
+// const nombre = "Pedro"
+// if(true){
+//     var nombre = "Lucas"
+//     console.log(nombre)
+// }
+
+// salida esperada Lucas
+
+// -----------------------------------------------------------------------------------------------------
+
+// Conventir las siguentes funcion en "funciones flecha"
+
+// function esNumeroPrimo(numero) {
+// 	if (numero <= 1) {
+// 		return false;
+// 	}
+// 	// Comprobar si el número es divisible por algún número menor que él mismo
+// 	for (let i = 2; i < numero; i++) {
+// 		if (numero % i === 0) {
+// 			return false;
+// 		}
+// 	}
+// 	return true;
+// }
+
+// function esPar(numero) {
+// 	if (numero % 2 === 0) {
+// 		return true;
+// 	} else {
+// 		return false;
+// 	}
+// }
+
+// function saludar() {
+// 	return 'Hola mundo!!';
+// }
+
+// -----------------------------------------------------------------------------------------------------
+
+// imprimir cada valor del siguiente array multiplicado
+// x2 utilizando map
+
+// const numeros = [10, 500, 1500, 2000];
+
+// salida -> [ 20, 1000, 3000, 4000 ]
+
+// 1- crear array solo con los clientes que tengan el status en true, usar filter
+// 2 - crear funcion flecha que permite buscar un cliente por el nombre, usar find
+// 3 - agregar un cliente utilizado push
+// const clientes = [
+// 	{ id: 12, nombre: 'Pedro', status: false },
+// 	{ id: 13, nombre: 'Sofia', status: true },
+// 	{ id: 14, nombre: 'Almendra', status: false },
+// 	{ id: 15, nombre: 'Carlos', status: true },
+// ];
+
+// DENTRO DE LA CARPETA DATA tiene un js que contiene un listado de usuario y un ejercicio,
+// importarlo y utilizar la funcion creada
